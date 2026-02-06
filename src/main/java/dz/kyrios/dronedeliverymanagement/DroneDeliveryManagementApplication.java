edd0cf1945820b73bd12445fb0ad554a37032063
abbes larbaoui <larbaouiabbes@gmail.com>
2026-02-06 20:08:07 +0300

    init the project

---
+++ src/main/java/dz/kyrios/dronedeliverymanagement/DroneDeliveryManagementApplication.java
@@ -0,0 +1,13 @@
+package dz.kyrios.dronedeliverymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDeliveryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDeliveryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dz/kyrios/dronedeliverymanagement/DroneDeleveryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.kyrios.dronedeliverymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDeleveryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
