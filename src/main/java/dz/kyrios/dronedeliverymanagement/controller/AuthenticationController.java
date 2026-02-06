d56dac396f3d88ee1473d4949200b2533e44752f
abbes larbaoui <larbaouiabbes@gmail.com>
2026-02-06 20:34:19 +0300

    implement authentication v1

---
+++ src/main/java/dz/kyrios/dronedeliverymanagement/controller/AuthenticationController.java
@@ -0,0 +1,33 @@
+package dz.kyrios.dronedeliverymanagement.controller;
+
+
+import dz.kyrios.dronedeliverymanagement.configuration.JwtService;
+import dz.kyrios.dronedeliverymanagement.dto.AuthenticationRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final JwtService jwtService;
+
+    public AuthenticationController(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/token")
+    public Map<String, String> getToken(@RequestBody AuthenticationRequest request) {
+
+        String token = jwtService.generateToken(
+                request.name(),
+                request.type()
+        );
+
+        return Map.of("token", token);
+    }
+}
