802a493a020ed245cf4b339fa4bff0aca22f6e18
abbes larbaoui <larbaouiabbes@gmail.com>
2026-02-09 20:24:05 +0300

    some unit tests for DroneService and OrderService

--- src/main/java/dz/kyrios/dronedeliverymanagement/service/OrderServiceImpl.java
+++ src/main/java/dz/kyrios/dronedeliverymanagement/service/OrderServiceImpl.java
@@ -10,12 +10,14 @@ import dz.kyrios.dronedeliverymanagement.dto.order.OrderResponse;
 import dz.kyrios.dronedeliverymanagement.mapper.OrderMapper;
 import dz.kyrios.dronedeliverymanagement.repository.OrderRepository;
 import dz.kyrios.dronedeliverymanagement.statics.OrderStatusStatic;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class OrderServiceImpl implements OrderService {
 
@@ -51,6 +53,7 @@ public class OrderServiceImpl implements OrderService {
         Order order = orderRepository.findByIdAndUsername(orderId, username);
 
         if (order == null) {
+            log.error("Get Order: Order with id {} not found", orderId);
             throw new NotFoundException("Order not found");
         }
 
@@ -72,9 +75,11 @@ public class OrderServiceImpl implements OrderService {
         Order order = orderRepository.findByIdAndUsername(orderId, username);
 
         if (order == null) {
+            log.error("Withdraw Order: Order with id {} not found", orderId);
             throw new NotFoundException("Order not found");
         }
         if (!validForWithdrawal(order)) {
+            log.error("Withdraw Order: Order with id {} is not valid", orderId);
             throw new RuntimeException("Can not withdraw order");
         }
         OrderStatus orderStatus = new OrderStatus();
@@ -94,6 +99,7 @@ public class OrderServiceImpl implements OrderService {
     public OrderResponse updateOriginDestination(String orderId, Location origin, Location destination) {
         Order order = orderRepository.findById(orderId);
         if (order == null) {
+            log.error("Update Origin Order: Order with id {} not found", orderId);
             throw new NotFoundException("Order not found");
         }
         order.setOrigin(origin);
